9bc97b2b1963025140cceaa1104b625dfc724172
Malika <majerrad@gmail.com>
2023-04-25 10:34:58 -0700

    printf

--- get_precision.c
+++ get_precision.c
@@ -10,43 +10,28 @@
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
-
 	int precision = -1;
 
-
-	if (format[curr_i] != '.')
-
-		return (precision);
-
-
-	precision = 0;
-
-
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	if (format[curr_i] == '%')
 	{
-
-		if (is_digit(format[curr_i]))
+		curr_i++;
+		while (format[curr_i] != '\0' && !is_conversion_specifier(format[curr_i]))
 		{
-
-			precision *= 10;
-
-			precision += format[curr_i] - '0';
-
-		}
-		else if (format[curr_i] == '*')
-		{
-
+			if (format[curr_i] == '.')
+			{
+				precision = 0;
+				curr_i++;
+				while (is_digit(format[curr_i]))
+				{
+					precision = precision * 10 + format[curr_i] - '0';
+					curr_i++;
+				}
+				break;
+			}
 			curr_i++;
-
-			precision = va_arg(list, int);
-
-			break;
-
 		}
-		else
-
-			break;
 	}
+
 	*i = curr_i - 1;
 	return (precision);
 }
