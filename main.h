7da32fde684e9d9fdbc72a22b7610504455e5f1a
Malika <majerrad@gmail.com>
2023-04-26 10:10:09 -0700

    printf

--- main.h
+++ main.h
@@ -38,7 +38,6 @@ struct fmt
  * @fm_t: The function associated.
  */
 typedef struct fmt fmt_t;
-
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
